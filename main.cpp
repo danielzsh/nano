13140aa6014873ec0510e05cc7e561b17d421234
dantheking-crypto <danielfangzhu@gmail.com>
2022-05-04 09:43:11 -0700

    Added .exe file

--- main.cpp
+++ main.cpp
@@ -6,9 +6,12 @@ std::string readFile(const std::string& filename)
     std::ifstream source(filename);
     return std::string((std::istreambuf_iterator<char>(source)), std::istreambuf_iterator<char>());
 }
-int main() {
-    std::string input = readFile("samples/sample2.nn");
-    Runner::Runner runner(input, false);
+int main(int argc, char** argv) {
+    bool jit = false;
+    std::string input;
+    if (argc > 1) input = readFile(argv[1]);
+    else jit = true;
+    Runner::Runner runner(input, jit);
     try {
         runner.run();
     } catch(std::string e) {
